7a0d5cabf9395b4114578d67fabbd08e5e9e282b
Cosmic Arrows <cosmicarrowsllc@Cosmics-MacBook-Pro.local>
2018-05-01 17:07:53 -0400

    Upload assets to Github from hardrive

---
+++ strobeLight6/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  strobeLight6
+//
+//  Created by Laurence Wingo on 1/17/13.
+//  Copyright (c) 2013 Laurence Wingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ strobeLight6/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  strobeLight6
+//
+//  Created by Laurence Wingo on 1/17/13.
+//  Copyright (c) 2013 Laurence Wingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
